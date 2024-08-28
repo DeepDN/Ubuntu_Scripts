a22c571f1ff478694f131337f296cbe9d3238a37
DeepDN <nemadedeepak1111@gmail.com>
2024-08-28 15:40:54 +0530

    Adding Zip installation script

---
+++ ZIP_FILE_INSTALLATION_SCRIPT.sh/ZIP_INSTALLATION_SCRIPT.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#update the system
+sudo apt update
+
+#Install ZIP software
+sudo apt-get install zip -y
+
+
+echo "Congratualation, ZIP is successfully installed on your system. Thank you, Deepak Nemade"
